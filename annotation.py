3d08fd09e87ab0b9b0e030083b96d8a2638c41d5
Mengfei Lan <39751535+MengfeiLan@users.noreply.github.com>
2026-04-20 03:23:34 -0500

    Update annotation.py

--- annotation.py
+++ annotation.py
@@ -152,7 +152,7 @@ if "ambiguous_referent_type" not in annotations.columns:
     
 @st.cache_data
 def load_data():
-    return pd.read_csv(DATA_PATH)[100:150].reset_index(drop=False)
+    return pd.read_csv(DATA_PATH)[0:150].reset_index(drop=False)
 
 # df = load_data()
 
@@ -167,7 +167,7 @@ def load_data_each_annotator(username):
     elif username == "shiwei":
         df = df.iloc[305:460]
     elif username == "mengfei":
-        df = df.iloc[:100]
+        df = df.iloc[:150]
     else:
         # fallback (e.g., halil or visitor)
         df = df.iloc[0:50]
