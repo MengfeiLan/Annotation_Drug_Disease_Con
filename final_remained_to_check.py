4f8585602e9ac10129a0a68b61d665f63b2ba719
Mengfei Lan <39751535+MengfeiLan@users.noreply.github.com>
2026-05-01 04:50:05 -0500

    Update final_remained_to_check.py

--- final_remained_to_check.py
+++ final_remained_to_check.py
@@ -608,8 +608,8 @@ try:
     df["shared_entities"] = df["shared_entities"].apply(ast.literal_eval)
     df["shared_text"] = df["shared_text"].apply(ast.literal_eval)
 except:
-    df["shared_entities"] = [] * len(df)
-    df["shared_text"] = [] * len(df)
+    df["shared_entities"] = [None] * len(df)
+    df["shared_text"] = [None] * len(df)
 row = df.iloc[st.session_state.current_idx]
 
 
